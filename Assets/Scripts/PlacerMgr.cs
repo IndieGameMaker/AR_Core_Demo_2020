7a2aed80d6fa1ecfa0b39d54d74b28c0a9fcc30a
Lee JaeHyun <myleje@gmail.com>
2020-12-05 09:46:46 +0900

    first commit

---
+++ Assets/Scripts/PlacerMgr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class PlacerMgr : MonoBehaviour
+{
+    public Camera arCamera;
+    public GameObject mummy;
+
+    void Start()
+    {
+        arCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        //첫번째 터치 정보
+        Touch touch = Input.GetTouch(0);
+
+        if (Input.touchCount < 1 || (touch.phase != TouchPhase.Began))
+        {
+            return;
+        } 
+
+        TrackableHit hit;
+        TrackableHitFlags filter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        
+        
+    }
+}
