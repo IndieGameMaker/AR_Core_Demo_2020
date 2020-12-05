527b0a9249ba5dd8f5e9a7a0b448296f785d6dd6
Lee JaeHyun <myleje@gmail.com>
2020-12-05 12:31:24 +0900

    .

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform arCamera;
+
+    void Start()
+    {
+        arCamera = GameObject.Find("First Person Camera").GetComponent<Transform>();        
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
