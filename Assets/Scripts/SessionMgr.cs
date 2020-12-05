c0cec74a8612b03be04e262d8dbbe9c12a8d8d96
Lee JaeHyun <myleje@gmail.com>
2020-12-06 08:33:15 +0900

    .

---
+++ Assets/Scripts/SessionMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+
+public class SessionMgr : MonoBehaviour
+{
+    public Text sessionInfo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        string tmp = "";
+
+        switch (Session.Status)
+        {
+            case SessionStatus.Initializing:
+                tmp = "Initializing...";
+                break;
+            case SessionStatus.Tracking:
+                tmp = "Tracking...";
+                break;
+            case SessionStatus.LostTracking:
+                tmp = "Lost Tracking!!!";
+                break;
+        }
+        sessionInfo.text = tmp;    
+    }
+
+}
