78bc7c4591853a8f81b127669691fc113fdf5a64
Lee JaeHyun <myleje@gmail.com>
2020-12-05 10:23:27 +0900

    .

---
+++ Assets/Scripts/MummyCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MummyCtrl : MonoBehaviour
+{
+    public Transform targetTr;
+    private Transform tr;
+
+    void Start()
+    {
+        targetTr = GameObject.Find("First Person Camera").GetComponent<Transform>();
+        tr = transform;
+    }
+
+    void Update()
+    {
+        //목적지로 회전
+        Vector3 dir = targetTr.position - tr.position;     
+        //쿼터니언 타입으로 변환
+        Quaternion rot = Quaternion.LookRotation(dir);
+        //부드럽게 회전 Slerp
+        tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * 3.0f);
+
+    }
+}
