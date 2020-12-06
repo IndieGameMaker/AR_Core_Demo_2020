63e6a1547ee804fbb861179a513e30f886efe231
Lee JaeHyun <myleje@gmail.com>
2020-12-06 10:27:02 +0900

    .

--- Assets/Scripts/MeasureMgr.cs
+++ Assets/Scripts/MeasureMgr.cs
@@ -13,16 +13,45 @@ public class MeasureMgr : MonoBehaviour
     private TrackableHit hit;
     //검출대상 설정
     private TrackableHitFlags flags = TrackableHitFlags.Default;
+    //터치 갯수
+    private int tapCount = 0;
+    //첫번째 터치한 좌표
+    private Vector3 firstPos = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Touch touch = Input.GetTouch(0);
+        if (Input.touchCount < 1 || touch.phase != TouchPhase.Began) return;
+
+        if (tapCount == 2) return;
+
+        //Arrow 생성하는 루틴
+        //레이캐스팅
+        if (Frame.Raycast(touch.position.x, touch.position.y, flags, out hit))
+        {
+            //터치 횟수를 증가
+            ++tapCount;
+
+            //앵커를 생성 Pose = x, y, z 위치와 각도
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            //앵커 하위에 Arrow 생성
+            Instantiate(markArrow, hit.Pose.position, Quaternion.identity, anchor.transform);
+
+            if (firstPos != Vector3.zero && tapCount == 2)
+            {
+                //길이 계산 & 출력
+                DisplayLength(firstPos, hit.Pose.position);
+            }
+            
+            firstPos = anchor.transform.position;
+        }
+    }
+
+    void DisplayLength(Vector3 prevPos, Vector3 currPos)
+    {
+        //Vector3.Distance(a, b)  --> a, b간의 거리
+        float length = Vector3.Distance(prevPos, currPos);
+        lenText.text = length.ToString("000.00") + "m";
     }
 }
